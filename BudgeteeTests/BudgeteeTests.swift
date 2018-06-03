7947c98089578078a09191de5b7139b9f5460951
Matvey Kravtsov <kravtsovguy@gmail.com>
2018-06-03 13:42:23 +0300

    some improvements

---
+++ BudgeteeTests/BudgeteeTests.swift
@@ -0,0 +1,23 @@
+//
+//  BudgeteeTests.swift
+//  BudgeteeTests
+//
+//  Created by Matvey Kravtsov on 03/06/2018.
+//  Copyright © 2018 Kravtsov. All rights reserved.
+//
+
+import XCTest
+
+class BudgeteeTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+    }
+}
